7b59ce041ff47258b5d4c21a8df44508068a792b
leonlin <leonlin14@gmail.com>
2016-08-23 14:43:06 +0800

    use react with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,53 @@
 var path = require('path');
 var webpack = require('webpack');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: {
         app: [
             path.join(__dirname, 'src', 'js', 'app.js')
-        ]
+        ],
+		vendors: [
+			'react',
+			'react-dom'
+		]
     },
     output: {
         path: path.join(__dirname, 'public', 'assets'),
         publicPath: '/assets/',
         filename: "bundle.js"
     },
+	plugins: [
+		new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.js'),
+		new webpack.NoErrorsPlugin()
+	],
     module: {
         loaders: [
+        	{
+				test: /\.js?$/,
+				loader: 'babel',
+				exclude: /(node_modules|bower_components)/,
+				query: {
+					presets: [ 'react' ]
+				}
+			},
             { test: /\.png$/,  loader: "url-loader?limit=1000" },
             { test: /\.jpg$/,  loader: "url-loader?limit=1000" },
             { test: /\.gif$/,  loader: "url-loader?limit=1000" },
             { test: /\.woff$/, loader: "url-loader?limit=1000" }
-        ]
+        ],
+		noParse: [
+			'react/dist/react.min.js',
+			'react-dom/dist/react-dom.min.js'
+		]
     },
     externals: {
         jQuery: true
-    }
+    },
+	resolve: {
+		alias: {
+			'react$': 'react/dist/react.min.js',
+			'react-dom$': 'react-dom/dist/react-dom.min.js'
+		}
+	}
 };
