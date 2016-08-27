2282aed42073380a7563a2b50525d549c936d7b2
leonlin <leonlin14@gmail.com>
2016-08-27 15:46:23 +0900

    update react router

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 class LandingPage extends React.Component {
 	render() {
 		return (
 			<div className="commentBox">
 				<h1>Hi! I am Landing Page.</h1>
+				<h2><Link to="/about">About</Link></h2>
 			</div>
 		);
 	}
--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import ReactDOM from'react-dom';
-import { Router, Route, browserHistory } from 'react-router';
+import { Router, Route, browserHistory, IndexRoute } from 'react-router';
 
 import LandingPage from '../components/LandingPage.jsx';
+import About from '../components/About.jsx';
+
+class App extends React.Component {
+	render() {
+		return (
+			<div>
+				{this.props.children}
+			</div>
+		)
+	}
+};
 
 ReactDOM.render((
 	<Router history={browserHistory}>
-		<Route path="/" component={LandingPage} />
+		<Route path="/" component={App}>
+			<IndexRoute component={LandingPage} />
+			<Route path="about" component={About} />
+		</Route>
 	</Router>
 ), document.getElementById('app'))
